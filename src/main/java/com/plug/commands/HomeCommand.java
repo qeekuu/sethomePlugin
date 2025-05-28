ba3af8163ec0a7d419b283ee63ae5cf8204e221f
domson <qeeu.que@gmail.com>
2025-05-28 14:47:44 +0200

    command classes

---
+++ src/main/java/com/plug/commands/HomeCommand.java
@@ -0,0 +1,51 @@
+package com.plug.multihome.commands;
+
+import com.example.multihome.MultiHomePlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+public class HomeCommand implements CommandExecutor {
+    private final MultiHomePlugin plugin;
+
+    public HomeCommand(MultiHomePlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Tylko gracze mogą używać tej komendy.");
+            return true;
+        }
+        if (args.length != 1) {
+            sender.sendMessage("Użycie: /home <nazwa>");
+            return true;
+        }
+        Player player = (Player) sender;
+        String homeName = args[0].toLowerCase();
+
+        FileConfiguration cfg = plugin.getHomesConfig();
+        String path = "players." + player.getUniqueId() + "." + homeName;
+        if (!cfg.contains(path)) {
+            player.sendMessage("Dom o nazwie '" + homeName + "' nie istnieje.");
+            return true;
+        }
+
+        String world = cfg.getString(path + ".world");
+        double x = cfg.getDouble(path + ".x");
+        double y = cfg.getDouble(path + ".y");
+        double z = cfg.getDouble(path + ".z");
+        float yaw = (float) cfg.getDouble(path + ".yaw");
+        float pitch = (float) cfg.getDouble(path + ".pitch");
+
+        Location loc = new Location(Bukkit.getWorld(world), x, y, z, yaw, pitch);
+        player.teleport(loc);
+        player.sendMessage("Teleportowano do domu '" + homeName + "'.");
+        return true;
+    }
+}
---
+++ src/main/java/com/plug/commands/SetHomeCommand.java
@@ -0,0 +1,45 @@
+package com.example.multihome.commands;
+import com.plug.multihome.MultiHomePlugin;
+
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.configuration.file.FileConfiguration;
+
+public class SetHomeCommand implements CommandExecutor {
+    private final MultiHomePlugin plugin;
+
+    public SetHomeCommand(MultiHomePlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Tylko gracze mogą ustawiać domy.");
+            return true;
+        }
+        if (args.length != 1) {
+            sender.sendMessage("Użycie: /sethome <nazwa>");
+            return true;
+        }
+        Player player = (Player) sender;
+        String homeName = args[0].toLowerCase();
+        Location loc = player.getLocation();
+
+        FileConfiguration cfg = plugin.getHomesConfig();
+        String path = "players." + player.getUniqueId() + "." + homeName;
+        cfg.set(path + ".world", loc.getWorld().getName());
+        cfg.set(path + ".x", loc.getX());
+        cfg.set(path + ".y", loc.getY());
+        cfg.set(path + ".z", loc.getZ());
+        cfg.set(path + ".yaw", loc.getYaw());
+        cfg.set(path + ".pitch", loc.getPitch());
+        plugin.saveHomesConfig();
+
+        player.sendMessage("Ustawiono dom '" + homeName + "'.");
+        return true;
+    }
+}
